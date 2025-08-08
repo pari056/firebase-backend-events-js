314ca4ad88eb2ae8ef711a63b0a6fb11db8d8fd9
Parikshit Rao <parikshit.rao@stackfinance.co>
2025-08-08 11:55:21 +0530

    Add Pino Logger for event logging

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 import fetch from "node-fetch";
 
 export class FirebaseEvents {
-  constructor(firebaseAppId, apiSecret) {
+  constructor(firebaseAppId, apiSecret, logger) {
     this.firebaseAppId = firebaseAppId;
     this.apiSecret = apiSecret;
+    this.logger = logger;
   }
 
   triggerEvent(payload) {
-    console.log('triggering firebase event')
-    console.log(payload)
+    // console.log('triggering firebase event')
+    // console.log(payload)
+    this.logger.info(`Triggering Firebase event`);
+    this.logger.child({'payload': payload});
+
     return fetch(
       `https://www.google-analytics.com/mp/collect?firebase_app_id=${this.firebaseAppId}&api_secret=${this.apiSecret}`,
       {
